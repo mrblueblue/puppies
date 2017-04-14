df2dfc24044badcd4252cd65e10dce938a194c4d
mrblueblue <grokut@gmail.com>
2017-04-13 20:35:32 -0700

    🐶🐶🐶🐶🐶🐶

---
+++ lib/beagle.ex
@@ -0,0 +1,18 @@
+defmodule Beagle do
+  @moduledoc """
+  Documentation for Beagle.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Beagle.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/beagle_test.exs
@@ -0,0 +1,8 @@
+defmodule BeagleTest do
+  use ExUnit.Case
+  doctest Beagle
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
